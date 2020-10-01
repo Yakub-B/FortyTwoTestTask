f6974ed14a8d625928b4ae8fb1333cffb2cccc67
bohdan <qq.0012@ukr.net>
2020-10-01 18:29:20 +0300

    t3: Created test for middleware (test fail).

--- apps/requests/tests.py
+++ apps/requests/tests.py
@@ -1,6 +1,5 @@
 from django.contrib.auth import get_user_model
-from django.test import TestCase
-
+from django.test import TestCase, Client
 
 # models tests
 from apps.requests.models import RequestModel
@@ -33,3 +32,31 @@ class RequestsModelTests(TestCase):
         self.assertEqual('utf-8', self.request.encoding)
         self.assertEqual('text/html', self.request.content_type)
         self.assertEqual(self.user_instance, self.request.user)
+
+
+# middleware tests
+
+
+class RequestLoggerMiddlewareTests(TestCase):
+    """
+    Tests for RequestLoggerMiddleware
+    """
+    @classmethod
+    def setUpTestData(cls):
+        user_model = get_user_model()
+        cls.user_instance = user_model.objects.create_user(
+            username='test', password='test'
+        )
+        client = Client()
+        client.force_login(cls.user_instance)
+        cls.response = client.get('/')
+
+    def test_request_logging(self):
+        """
+        Testing if request is saved into db
+        """
+        request_instance = RequestModel.objects.get(user=self.user_instance)
+        self.assertEqual('http://testserver/', request_instance.url)
+        self.assertEqual('GET', request_instance.method)
+        self.assertEqual(None, request_instance.encoding)
+        self.assertEqual('', request_instance.content_type)
