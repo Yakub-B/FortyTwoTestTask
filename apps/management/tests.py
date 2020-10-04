37b783de57fc27cd9848e7499dc9ed7a64aca24f
bohdan <qq.0012@ukr.net>
2020-10-04 18:25:43 +0300

    ticket:8 Created test for db action model.

--- apps/management/tests.py
+++ apps/management/tests.py
@@ -1,6 +1,8 @@
+from django.contrib.auth import get_user_model
 from django.test import TestCase
 
 from apps.hello.models import ProfileModel
+from apps.management.models import DataBaseActionModel
 
 
 class AdminEditLinkTagTests(TestCase):
@@ -18,3 +20,28 @@ class AdminEditLinkTagTests(TestCase):
         Looking if response contains valid link to admin edit object page
         """
         self.assertContains(self.response, '<a href="/admin/hello/profilemodel/1/change/">(admin)</a>', html=True)
+
+
+class DataBaseActionModelTests(TestCase):
+    def setUp(self):
+        # getting user created by data migration
+        user_model = get_user_model()
+        user = user_model.objects.first()
+        # making some changes with user
+        user.username = 'test'
+        user.save()
+        self.entry = DataBaseActionModel.objects.first()
+
+    def test_entries_from_data_migration(self):
+        """
+        Testing creation of action entry
+        """
+        self.assertEqual('django.contrib.auth.models', self.entry.app)
+        self.assertEqual(DataBaseActionModel.Action.EDITING, self.entry.action)
+
+    def test_entry_does_not_created_for_db_action_model(self):
+        """
+        Testing if listeners don`t create entries about DataBaseActionModel actions
+        """
+        # 2 entries created with data migration (ProfileModel, User)
+        self.assertEqual(1, DataBaseActionModel.objects.count())
