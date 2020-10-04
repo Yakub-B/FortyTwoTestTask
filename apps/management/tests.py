83832606767975adec396284cdc23dd73cb21158
bohdan <qq.0012@ukr.net>
2020-10-04 17:42:57 +0300

    ticket:8 Created test for template command (test fail).

--- apps/management/tests.py
+++ apps/management/tests.py
@@ -1,3 +1,12 @@
 from django.test import TestCase
 
-# Create your tests here.
+
+class AdminEditLinkTagTests(TestCase):
+    def setUp(self):
+        self.response = self.client.get('/')
+
+    def test_tag_returns_valid_href(self):
+        """
+        Looking if response contains valid link to admin edit object page
+        """
+        self.assertContains(self.response, '<a href="/admin/hello/profilemodel/1/change/">(admin)</a>', html=True)
