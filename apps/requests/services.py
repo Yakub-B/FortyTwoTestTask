94e4db4aef1e1becb95d7806f6c66d066749f15a
bohdan <qq.0012@ukr.net>
2020-10-05 17:40:13 +0300

    ticket:13 Separate LastTenRequestView.get into 2 functions because of high complexity (flake8 C901).

---
+++ apps/requests/services.py
@@ -0,0 +1,42 @@
+from django.core.exceptions import ObjectDoesNotExist
+from django.http import Http404, JsonResponse
+from django.shortcuts import render
+
+from apps.requests.models import RequestModel
+
+
+def last_ten_requests_not_ajax(request):
+    try:
+        latest_request_id = RequestModel.objects.all().latest().id
+    except ObjectDoesNotExist:
+        raise Http404
+
+    if request.GET.get('priority'):
+        priority = request.GET.get('priority')
+        qs = RequestModel.objects.filter(priority=priority)[:10]
+    else:
+        qs = RequestModel.objects.all()[:10]
+
+    context = {'requests': qs, 'latest_request_id': latest_request_id}
+    return render(request, 'requests.html', context)
+
+
+def last_ten_requests_ajax(request):
+    # in get parameter 'id' we get id of the last request displayed
+    # on page
+    if request.GET.get('id'):
+        # looking for newer requests
+        # (with id grater then id we got from ajax)
+        new_qs = RequestModel.objects.filter(
+            id__gt=request.GET['id']).order_by('priority', 'timestamp')[:10]
+        if new_qs.exists():
+            # serializing queryset
+            data = list(new_qs.values())
+            latest_request_id = data[-1]['id']
+            data = {
+                'requests': data, 'latest_request_id': latest_request_id
+            }
+            return JsonResponse(data)
+        else:
+            return JsonResponse({'data': None})
+    raise Http404
--- apps/requests/views.py
+++ apps/requests/views.py
@@ -1,51 +1,19 @@
-from django.core.exceptions import ObjectDoesNotExist
-from django.http import Http404, JsonResponse
-from django.shortcuts import render
 from django.views.generic.base import View
 
-from apps.requests.models import RequestModel
+from apps.requests.services import last_ten_requests_not_ajax, last_ten_requests_ajax
 
 
 class LastTenRequestsView(View):
     """
-       This view will render page with last 10 requests
-       and process ajax requests to get newer requests from db
-       """
+    This view will render page with last 10 requests
+    and process ajax requests to get newer requests from db
+    """
 
     def get(self, request):
         """
         Main method that process get requests, both ajax and common
         """
         if not request.is_ajax():
-            try:
-                latest_request_id = RequestModel.objects.all().latest().id
-            except ObjectDoesNotExist:
-                raise Http404
-
-            if request.GET.get('priority'):
-                priority = request.GET.get('priority')
-                qs = RequestModel.objects.filter(priority=priority)[:10]
-            else:
-                qs = RequestModel.objects.all()[:10]
-
-            context = {'requests': qs, 'latest_request_id': latest_request_id}
-            return render(request, 'requests.html', context)
+            return last_ten_requests_not_ajax(request)
         else:
-            # in get parameter 'id' we get id of the last request displayed
-            # on page
-            if request.GET.get('id'):
-                # looking for newer requests
-                # (with id grater then id we got from ajax)
-                new_qs = RequestModel.objects.filter(
-                    id__gt=request.GET['id']).order_by('priority', 'timestamp')[:10]
-                if new_qs.exists():
-                    # serializing queryset
-                    data = list(new_qs.values())
-                    latest_request_id = data[-1]['id']
-                    data = {
-                        'requests': data, 'latest_request_id': latest_request_id
-                    }
-                    return JsonResponse(data)
-                else:
-                    return JsonResponse({'data': None})
-            raise Http404
+            return last_ten_requests_ajax(request)
